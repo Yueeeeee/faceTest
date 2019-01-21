a78346c4edfd3b99a4bbc6ebb3c3be771f17f34a
yves <yves@Yves-MacBook-Pro.local>
2019-01-21 17:36:34 +0800

    try to change facingMode: use

--- utils.js
+++ utils.js
@@ -127,7 +127,7 @@ function Utils(errorOutputId) { // eslint-disable-line no-unused-vars
             videoConstraint = true;
         }
 
-        navigator.mediaDevices.getUserMedia({video: videoConstraint, audio: false})
+        navigator.mediaDevices.getUserMedia({video: videoConstraint,video:{ 'facingMode': "user" }, audio: false})
             .then(function(stream) {
                 video.srcObject = stream;
                 video.play();
