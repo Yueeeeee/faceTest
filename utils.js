b727b9af8be453b5e934e701cb0d149969323ed1
yves <yves@Yves-MacBook-Pro.local>
2019-01-22 09:24:53 +0800

    change constraints, adapter version

--- utils.js
+++ utils.js
@@ -116,7 +116,7 @@ function Utils(errorOutputId) { // eslint-disable-line no-unused-vars
         // const constraints = {
         //     'qvga': {width: {exact: 320}, height: {exact: 240}},
         //     'vga': {width: {exact: 640}, height: {exact: 480}}};
-        const constraints = { width:160,height:120 };
+        const constraints = { width:320,height:240 };
         let video = document.getElementById(videoId);
         if (!video) {
             video = document.createElement('video');
@@ -127,7 +127,7 @@ function Utils(errorOutputId) { // eslint-disable-line no-unused-vars
             videoConstraint = true;
         }
 
-        navigator.mediaDevices.getUserMedia({video: videoConstraint,video:{ 'facingMode': "user" }, audio: false})
+        navigator.mediaDevices.getUserMedia({video: videoConstraint, audio: false})
             .then(function(stream) {
                 video.srcObject = stream;
                 video.play();
